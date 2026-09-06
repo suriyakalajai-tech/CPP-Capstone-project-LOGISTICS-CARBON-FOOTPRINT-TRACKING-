58f296841776ac8e5e40373aa2ea5fdf0f7fa9eb
suriyakalajai-tech <suriyakalajai@gmail.com>
2026-09-06 18:47:40 +0530

    Delete middleware directory

--- middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error('[LOGIFLOW SERVER ERROR]:', err.stack);
-  res.status(err.status || 500).json({
-    success: false,
-    error: err.message || 'Internal Server Error'
-  });
-};
